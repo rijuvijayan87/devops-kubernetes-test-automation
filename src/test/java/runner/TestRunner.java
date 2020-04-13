df8346cf09e14bbfab3adf20249f725eced1e24c
rijuvijayan <rijuvijayan@gmail.com>
2020-04-13 23:30:43 +1200

    parallel execution done

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,16 +2,23 @@ package runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"src/test/java/features/train-schedule.feature"},
         glue = {"features.stepDefinitions"},
         monochrome = true,
         strict = true,
-        plugin = {"json:target/cucumber-report-feature-composite.json"},
+        plugin = {"json:target/cucumber-report/cucumber.json"},
         dryRun = false,
         tags = {""}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
